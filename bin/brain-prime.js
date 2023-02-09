46a3f1481226666170b21a579b3ab8f6ab5789b3
NEK1FEKS <nikita-pol98@mail.ru>
2023-02-09 14:36:58 +0300

    Add: brain-prime.js, code-prime.js. Modified: Makefile, README.md, package.json

---
+++ bin/brain-prime.js
@@ -0,0 +1,5 @@
+#!/usr/bin/env node
+
+import primeNumbers from '../src/games/code-prime.js';
+
+primeNumbers();
---
+++ src/games/code-prime.js
@@ -0,0 +1,35 @@
+import readlineSync from 'readline-sync';
+import greetingUserName from './cli.js';
+
+const primeNumbers = () => {
+  const userName = greetingUserName();
+  console.log('Answer "yes" if given number is prime. Otherwise answer "no".');
+  let curCorrect = 0;
+
+  for (let i = 0; i < 3; i += 1, curCorrect += 1) {
+    const randomNum = Math.floor(Math.random() * 100);
+    let fail = 0;
+    for (let j = 2; j < randomNum; j += 1) {
+      if (randomNum % j === 0) {
+        fail -= 1;
+        break;
+      }
+    }
+    const answer = readlineSync.question(`${'Question: '}${randomNum}${'\nYour answer: '}`);
+    if ((fail === -1 && answer !== 'no') || (randomNum === 0 && answer !== 'no')) {
+      console.log(`'${answer}' is wrong answer ;(. Correct answer was 'no'.`);
+      console.log(`Let's try again, ${userName}!`);
+      break;
+    } else if (fail === 0 && answer !== 'yes') {
+      console.log(`'${answer}' is wrong answer ;(. Correct answer was 'yes'.`);
+      console.log(`Let's try again, ${userName}!`);
+      break;
+    }
+    console.log('Correct!');
+  }
+  if (curCorrect === 3) {
+    console.log(`Congratulations, ${userName}!`);
+  }
+};
+
+export default primeNumbers;
